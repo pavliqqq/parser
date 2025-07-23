c8b4c377ee99269236ab29adf8e2ab58e3a647b8
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-23 17:27:22 +0000

    edit AbstractScrapper: add method moveToErrorQueue

--- app/Scrappers/AbstractScrapper.php
+++ app/Scrappers/AbstractScrapper.php
@@ -13,13 +13,25 @@ abstract class AbstractScrapper implements ScrapperInterface
     protected LinkService $linkService;
     protected DataService $dataService;
     protected Logger $logger;
-    public function __construct(DocumentService $documentService, LinkService $linkService, DataService $dataService, Logger $logger)
-    {
+    public function __construct(
+        DocumentService $documentService,
+        LinkService $linkService,
+        DataService $dataService,
+        Logger $logger
+    ) {
         $this->documentService = $documentService;
         $this->linkService = $linkService;
         $this->dataService = $dataService;
         $this->logger = $logger;
     }
 
-    abstract public function run(string $url): array;
+    abstract public function run(string $url): void;
+
+    public function moveToErrorQueue(string $url): void
+    {
+        $this->linkService->moveLinkToErrorQueue([
+            'url' => $url,
+            'class' => static::class
+        ]);
+    }
 }
