72a438cfeaae665306d85554a81b4b76a3965343
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-07-23 17:40:05 +0000

    edit methods addLink, getLink, returnLink, add moveLinkToErrorQueue

--- app/Services/LinkService.php
+++ app/Services/LinkService.php
@@ -13,65 +13,101 @@ class LinkService
     protected Logger $logger;
     protected Client $client;
     protected string $baseUrl;
-
-    protected string $queueTableName;
+    protected string $links;
+    protected string $processedLinks;
+    protected string $errorLinks;
 
     public function __construct(Redis $redis, Logger $logger, Client $client, string $baseUrl)
     {
+        $config = require __DIR__ . '/../../config/redis.php';
+        $this->links = $config['queue']['links'];
+        $this->processedLinks = $config['queue']['processed'];
+        $this->errorLinks = $config['queue']['error'];
+
         $this->redis = $redis;
         $this->logger = $logger;
         $this->client = $client;
         $this->baseUrl = $baseUrl;
     }
 
-    public function addLink(string $key, array $urls): void
+    public function addLink(string $queue, array $rowLink): void
     {
-        foreach ($urls as $url) {
-            $link = $this->baseUrl . '/' . $url['url'];
-            try {
-                $client = $this->redis->getClient();
-                $result = false;
-                if (!$client->sismember('urls', $link)) {
-                    $client->sadd('urls', [$link]);
-                    $client->rpush($key, [json_encode([
-                        'url' => $link,
-                        'class' => $url['class'],
-                    ])]);
-                    $result = true;
-                }
-                if ($result) {
-                    $this->logger->info("Insert link: $link");
-                } else {
-                    $this->logger->error("This link is already in list");
-                }
-            } catch (Exception $e) {
-                $this->logger->error("Insert link error", [
-                    'message' => $e->getMessage(),
-                    'trace' => $e->getTraceAsString()]);
+        $rowLink['url'] = $this->baseUrl . '/' . $rowLink['url'];
+        $link = $rowLink['url'];
+
+        try {
+            if (!$this->redis->sIsMember('urls', $link)) {
+                $this->redis->sAdd('urls', [$link]);
+                $this->redis->rPush($queue, $rowLink);
+
+                $this->logger->info("Insert link: $link");
+            } else {
+                $this->logger->info("This link is already in list");
             }
+        } catch (Exception $e) {
+            $this->logger->error("Error while inserting link to queue '$queue'", [
+                'url' => $link,
+                'message' => $e->getMessage()
+            ]);
         }
     }
 
-    public function getLink()
+    public function getLink() :?array
     {
         try {
-            $client = $this->redis->getClient();
-            $rawLink = $client->lpop('urlsQueue');
-
-            if ($rawLink) {
-                $link = json_decode($rawLink, true);
-
-                $client->rpush('processedUrls', [json_encode($link)]);
-                $this->logger->info('Get link: ' . $link['url']);
-                return $link;
+            $rowLink = $this->redis->pop($this->links);
+            if ($rowLink) {
+                $this->redis->rPush($this->processedLinks, $rowLink);
+                $this->logger->info('Get link: ' . $rowLink['url']);
+                return $rowLink;
             } else {
-                $this->logger->error("This link is missing in queue");
+                $this->logger->warning("The queue '{$this->links}' is empty");
                 return null;
             }
         } catch (Exception $e) {
-            $this->logger->error('Get link error', [
-                'message' => $e->getMessage(),
-                'trace' => $e->getTraceAsString()]);
+            $this->logger->error('Error while getting link', [
+                'message' => $e->getMessage()
+            ]);
+        }
+        return null;
+    }
+
+    public function returnLink(array $link): void
+    {
+        try {
+            $removed = $this->redis->removeElement($this->processedLinks, $link);
+
+            if ($removed) {
+                $this->logger->warning("Removed from '{$this->processedLinks}': {$link['url']}");
+
+                $this->redis->lPush($this->links, $link);
+
+                $this->logger->info("Returned link back to '{$this->links}': {$link['url']}");
+            } else {
+                $this->logger->warning("Link not found in '{$this->processedLinks}': {$link['url']}");
+            }
+        } catch (Exception $e) {
+            $this->logger->error('Error while returning link', [
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function moveLinkToErrorQueue(array $link): void
+    {
+        try {
+            $removed = $this->redis->removeElement($this->processedLinks, $link);
+
+            if ($removed) {
+                $this->redis->rPush($this->errorLinks, $link);
+                $this->logger->info("Link moved to '{$this->errorLinks}': {$link['url']}");
+            } else {
+                $this->logger->warning("Link not found in '{$this->processedLinks}': {$link['url']}");
+            }
+        } catch (Exception $e) {
+            $this->logger->error('Error while moving link', [
+                'message' => $e->getMessage()
+            ]);
         }
     }
 }
