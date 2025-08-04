66965a3487d576a183c77003a1e41cc341ad0273
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-08-04 16:43:27 +0000

    add methods: getId, insertData, linkQuestionToAnswer, rename method insertData to insertQuestionsAndAnswers, edit method insertQuestionsAndAnswers

--- app/Services/DataService.php
+++ app/Services/DataService.php
@@ -10,51 +10,97 @@ class DataService
 {
     protected Database $dataBase;
     protected Logger $logger;
-    protected string $dataTableName;
+    protected string $questionsTableName;
+    protected string $answersTableName;
+    protected string $questionAnswerTableName;
 
     public function __construct(Database $dataBase, Logger $logger)
     {
         $config = require __DIR__ . '/../../config/db.php';
 
-        $this->dataTableName = $config['data_table']['name'];
+        $this->questionsTableName = $config['data_table']['questions'];
+        $this->answersTableName = $config['data_table']['answers'];
+        $this->questionAnswerTableName = $config['data_table']['question_answer'];
 
         $this->dataBase = $dataBase;
         $this->logger = $logger;
     }
 
-    public function insertData(array $rows): void
+    public function insertQuestionsAndAnswers(array $rows): void
     {
+        $this->dataBase->beginTransaction();
         try {
             $columns = array_keys($rows[0]);
-            $columnCount = count($columns);
 
-            $placeholders = [];
-            $values = [];
+            $questionColumn = $columns[0];
+            $answerColumn = $columns[1];
 
             foreach ($rows as $row) {
-                $placeholders[] = '(' . rtrim(str_repeat('?,', $columnCount), ',') .')';
+                $questionData = $row[$questionColumn];
+                $answerData = $row[$answerColumn];
 
-                foreach ($columns as $column) {
-                    $values[] = $row[$column];
-                }
+                $questionId = $this->getId($this->questionsTableName, $columns[0], $questionData);
+                $answerId = $this->getId($this->answersTableName, $columns[1], $answerData);
+
+                $data = [
+                    'question_id' => $questionId,
+                    'answer_id' => $answerId,
+                ];
+
+                $this->linkQuestionToAnswer($data);
             }
-
-            $sql = sprintf(
-                "INSERT INTO %s (%s) VALUES %s",
-                $this->dataTableName,
-                implode(', ', $columns),
-                implode(', ', $placeholders)
-            );
-
-            $stmt = $this->dataBase->query($sql, $values);
-            $rowsCount = $stmt->rowCount();
-
-            $this->logger->info("Insert rows: $rowsCount");
+            $this->dataBase->commit();
         } catch (PDOException $e) {
+            $this->dataBase->rollback();
             $this->logger->error("Insert data error", [
-                'message' => $e->getMessage(),
-                'trace' => $e->getTraceAsString()
+                'message' => $e->getMessage()
             ]);
         }
+    }
+
+    private function getId(string $table, string $column, string $data)
+    {
+        $sql = sprintf(
+            "SELECT id FROM %s WHERE %s = ?",
+            $table,
+            $column
+        );
+
+        $stmt = $this->dataBase->query($sql, [$data]);
+
+        $id = $stmt->fetchColumn();
+
+        if (!$id) {
+            return $this->insertData($table, $column, $data);
+        }
+
+        return $id;
+    }
+
+    private function insertData(string $table, string $column, string $data): string
+    {
+        $sql = sprintf(
+            "INSERT INTO %s (%s) VALUES (?)",
+            $table,
+            $column
+        );
+
+        $this->dataBase->query($sql, [$data]);
+        return $this->dataBase->lastInsertId();
+    }
+
+    private function linkQuestionToAnswer(array $data): void
+    {
+        $columns = array_keys($data);
+        $placeholders = array_map(fn($col) => "?", $columns);
+
+        $sql = sprintf(
+            "INSERT IGNORE INTO %s (%s) VALUES (%s)",
+            $this->questionAnswerTableName,
+            implode(', ', $columns),
+            implode(', ', $placeholders)
+        );
+
+        $this->dataBase->query($sql, array_values($data));
     }
 }
